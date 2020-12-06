e3a8382faa6941a20dfe9691cf2dc09c78bf7847
Andreas Houben <andreas.houben@codecentric.de>
2020-12-06 14:27:53 +0100

    Remove log

--- src/2/index.ts
+++ src/2/index.ts
@@ -8,6 +8,5 @@ export const solveDoor02 = (data: string): number => {
   const numbers = convertToArrayOfNumbers(data)
   const permutated = permutateArray(numbers, 3)
   const match = findNumbersOnArrayThatAddUpTo(permutated, 2020)
-  console.log(match)
   return match.reduce((a, b) => a * b)
 }
