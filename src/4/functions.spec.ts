eb52ae2801e84c7b66e39933e3d9f17b21cbed5c
Andreas Houben <andreas.houben@codecentric.de>
2020-12-10 22:33:43 +0100

    door04

--- src/4/functions.spec.ts
+++ src/4/functions.spec.ts
@@ -1,4 +1,13 @@
-import {isPassportValid, parseDataIntoPassportArray} from './functions'
+import {
+  isPassportValid,
+  isValidEyeColor,
+  isValidHairColor,
+  isValidHeight,
+  isValidPid,
+  isValidYear,
+  parseDataIntoPassportArray,
+} from './functions'
+import each from 'jest-each'
 
 describe('parseDataIntoPassportArray', () => {
   it('should create a new group after every emptyline', () => {
@@ -43,5 +52,117 @@ hcl:#cfa07d byr:1929`
 iyr:2011 ecl:brn hgt:59in`
 
     expect(isPassportValid(passport)).toBe(false)
+  })
+
+  each([
+    [
+      `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f`,
+    ],
+    [
+      `eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm`,
+    ],
+    [
+      `hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022`,
+    ],
+  ]).it('%s should be accepted', (passport) => {
+    expect(isPassportValid(passport)).toBe(true)
+  })
+
+  each([
+    [
+      `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926`,
+    ],
+    [
+      `iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946`,
+    ],
+    [
+      `hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277`,
+    ],
+    [
+      `hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`,
+    ],
+  ]).it('%s should be rejected', (passport) => {
+    expect(isPassportValid(passport)).toBe(false)
+  })
+})
+
+describe('isValidYear', () => {
+  each([
+    ['1919', 1920, 2020, false],
+    ['1920', 1920, 2020, true],
+    ['2020', 1920, 2020, true],
+    ['202b', 1920, 2020, false],
+  ]).it(
+    'should return for %s between %d and $d: $p ',
+    (value, min, max, expected) => {
+      const actual = isValidYear(value, min, max)
+
+      expect(actual).toBe(expected)
+    },
+  )
+})
+
+describe('isValidHeight', () => {
+  each([
+    [false, '149cm'],
+    [true, '150cm'],
+    [true, '193cm'],
+    [false, '194cm'],
+    [false, '58in'],
+    [true, '59in'],
+    [true, '76in'],
+    [false, '77in'],
+    [false, '72'],
+    [false, '75'],
+  ]).it('should return %p for %s', (expected, value) => {
+    const actual = isValidHeight(value)
+
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('isValidHairColor', () => {
+  each([
+    [true, '#123abc'],
+    [false, '#123abz'],
+    [false, '#123abcd'],
+    [false, '#123ab'],
+    [false, '123abc'],
+  ]).it('should return %p for %d', (expected, value) => {
+    const actual = isValidHairColor(value)
+
+    expect(actual).toBe(expected)
+  })
+})
+
+describe('isValidEyeColor', () => {
+  each([['amb'], ['blu'], ['brn'], ['gry'], ['grn'], ['hzl'], ['oth']]).it(
+    'should return true for %s',
+    (value) => {
+      expect(isValidEyeColor(value)).toBe(true)
+    },
+  )
+})
+
+describe('isValidPid', () => {
+  each([
+    [true, '000000001'],
+    [false, '0123456789'],
+    [false, '00000002'],
+  ]).it('should return %p for %s', (expected, value) => {
+    const actual = isValidPid(value)
+
+    expect(actual).toBe(expected)
   })
 })
--- src/4/functions.ts
+++ src/4/functions.ts
@@ -7,17 +7,61 @@ export function parseDataIntoPassportArray(data: string): Passport[] {
 export function isPassportValid(passport: Passport): boolean {
   const necessaryFields = ['ecl', 'pid', 'eyr', 'hcl', 'byr', 'iyr', 'hgt']
 
-  const fieldsFound = passport
+  const fieldObject: {[key: string]: string} = {}
+
+  passport
     .replace(/\n/g, ' ')
     .split(' ')
-    .map((field) => {
-      const match = /(.*):.*/.exec(field)
-      return match && match[1] ? match[1] : ''
+    .map((value) => value.match(/(.*):(.*)/))
+    .forEach((match) => {
+      if (match) {
+        fieldObject[match[1]] = match[2]
+      }
     })
 
   return (
     necessaryFields
-      .map((field) => fieldsFound.includes(field))
+      .map((field) => Object.keys(fieldObject).includes(field))
       .filter((val) => !val).length === 0
   )
+}
+
+export function isValidYear(value: string, min: number, max: number): boolean {
+  try {
+    const number = parseInt(value)
+    return number >= min && number <= max
+  } catch {
+    return false
+  }
+}
+
+export function isValidHeight(value: string): boolean {
+  const exp = /(\d*)(\w*)/
+  if (exp.test(value)) {
+    const exec = value.match(exp)
+    const height = exec ? parseInt(exec[1]) : null
+    const unit = exec ? exec[2] : null
+    if (!height || !unit) {
+      return false
+    }
+    if (unit === 'cm') {
+      return height >= 150 && height <= 193
+    }
+    if (unit === 'in') {
+      return height >= 59 && height <= 76
+    }
+  }
+  return false
+}
+
+export function isValidHairColor(value: string): boolean {
+  return /^#((\d|[a-f]){6})$/.test(value)
+}
+
+export function isValidEyeColor(value: string): boolean {
+  return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value)
+}
+
+export function isValidPid(value: string): boolean {
+  return /^\d{9}$/.test(value)
 }
